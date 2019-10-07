ee83b23788d2524c3a45573a16a8beaafab00089
Zainchkovskyy Artem <zainchkovskyy.artem@gmail.com>
2019-10-08 00:55:06 +0300

    Add csv seed data files.

---
+++ Scheduler_test/Domain/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler_test.Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public virtual IList<Meeting> Meetings { get; set; }
+    }
+}
---
+++ Scheduler_test/Domain/Meeting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scheduler_test.Domain
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        public DateTime ScheduledTime { get; set; }
+        public virtual Employee Employee { get; set; }
+    }
+}
